778f9bebd92cbc254bfd597bcf11356d286b51ff
yara22aulia <yara22aulia@gmail.com>
2026-04-16 18:15:55 +0700

    Mengimplementasikan logika swap pada bubble sort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -37,12 +37,15 @@ void input(
     }
 }
 
-void bubbleSortArray()
+void bubbleSortArray(
+
+)
 {
     int pass = 1;
     do
     {
-        for (int j = 0; j <= nPanjang -1 - pass; j++)
+        for (int j = 0; j <= nPanjang -1 - pass; j++
+        )
         {
             if (element[j] > element[j + 1])
             {
@@ -54,5 +57,70 @@ void bubbleSortArray()
         }
         pass++;
     } while (pass <= nPanjang - 1);
+    
+}
+
+void display()
+{
+    cout << "\n===================================\n";
+    cout << " Elemen Array Setelah Diurutkan (Asc)\n";
+    cout << "=====================================\n";
+
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
+
+
+void binarrySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n=================================\n";
+        cout << "       Pencarian Binary Search    \n";
+        cout << "=================================\n";
+
+        cout << "Masukan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
+
+        do
+        {
+            int mid = (low + high) / 2;
+            
+            if (element[mid] == x)
+            {
+                cout << "\n[T] Elemen " << x << "ditemukan pada indeks " << "\n";
+                return;
+            }
+
+            if (x < element[mid])
+            {
+                high = mid -1;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
+        } while (low <= high);
+
+        if (low > high)
+        {
+            cout << "\n[x] Elemen " << x << "tidak ditemukan dalam array. \n";
+        }
+
+        cout << "\nIngin mencari lagi? (y/n): ";
+        cin >> ulang;
+        
+    } while (ulang == 'y' || ulang == 'Y');
     
 }
