0ba07a1589564b5e1a23437323fed613f0958773
yara22aulia <yara22aulia@gmail.com>
2026-04-16 18:03:36 +0700

    Menambahkan validasi jumlah elemen array maksimal 10

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -5,7 +5,9 @@ int element[10];
 int nPanjang;
 int x;
 
-void input()
+void input(
+    
+)
 {
     while (true)
     {
@@ -32,22 +34,3 @@ void input()
     }
 }
 
-void bubbleSortArray()
-{
-    int pass = 1;
-    do
-    {
-        for (int j = 0; j <= nPanjang -1 - pass; j++)
-        {
-            if (element[j] > element[j + 1])
-            {
-              int temp = element[j];
-              element[j] = element[j + 1];
-              element[j + 1] = temp;
-            }
-             
-        }
-        pass++;
-    } while (pass <= nPanjang - 1);
-    
-}
