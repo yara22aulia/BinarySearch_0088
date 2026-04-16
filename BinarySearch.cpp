17705f948fee716445d9d07cac79e19a5a433132
yara22aulia <yara22aulia@gmail.com>
2026-04-16 17:31:47 +0700

    first commit

---
+++ BinarySearch.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout <<"Masukkan banyaknya elemen pada array (maksimal 10): ";
+        cin >> nPanjang;
+        if (nPanjang <=10)
+        {
+            break;
+        }
+        else
+        {
+            cout <<"\n[!]Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+    }
+
+    cout << "\n=============================\n";
+    cout << "       Masukan Elemen Array    \n";
+    cout << "===============================\n";
+
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke -" << (i + 1) << " = ";
+        cin >> element[i];
+    }
+}
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int j = 0; j <= nPanjang -1 - pass; j++)
+        {
+            if (element[j] > element[j + 1])
+            {
+              int temp = element[j];
+              element[j] = element[j + 1];
+              element[j + 1] = temp;
+            }
+             
+        }
+        pass++;
+    } while (pass <= nPanjang - 1);
+    
+}
